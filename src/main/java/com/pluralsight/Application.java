879065cc20718b6acc196f23b918af8b4a658a19
edomg <edomgezahegn54@gmail.com>
2026-05-22 15:34:54 -0500

    Set up Delicious-sandwitch-capstone-2 project structure

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -2,6 +2,6 @@ package com.pluralsight;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Welcome to DELI-cious!");
+        System.out.println("Delicious-sandwitch-capstone-2!");
     }
 }
