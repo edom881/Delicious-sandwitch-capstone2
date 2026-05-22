893b271450ccca63146ef69fc0aadb296a996995
edomg <edomgezahegn54@gmail.com>
2026-05-22 15:45:04 -0500

    I Add scanner to ask a user for input

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -1,6 +1,11 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Application {
+
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Delicious-sandwitch-capstone-2!");
     }
