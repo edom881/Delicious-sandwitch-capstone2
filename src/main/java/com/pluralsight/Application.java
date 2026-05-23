afffe454fe8aaa5ce74dfbd281f58aff85e4df1c
edomg <edomgezahegn54@gmail.com>
2026-05-23 13:26:38 -0500

    Build home screen menu and application loop

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -7,6 +7,34 @@ public class Application {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Delicious-sandwitch-capstone-2!");
+        boolean running = true;
+
+        while (running) {
+
+            System.out.println("\n=== DELI-cious Sandwich Shop ===");
+            System.out.println("1) New Order");
+            System.out.println("0) Exit");
+            System.out.print("Enter choice: ");
+
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+
+                case "1":
+                    System.out.println("Starting new order...");
+                    break;
+
+                case "0":
+                    running = false;
+                    System.out.println("Thank you for visiting DELI-cious!");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Try again.");
+            }
+        }
     }
-}
+
+    }
+
+
