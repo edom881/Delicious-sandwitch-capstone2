3f95bc634c8caf7a1304bf47791681c3eccc2809
edomg <edomgezahegn54@gmail.com>
2026-05-23 13:53:20 -0500

    create sandwich model class

---
+++ src/main/java/com/pluralsight/models/Sandwich.java
@@ -0,0 +1,18 @@
+package com.pluralsight.models;
+
+public class Sandwich {
+    private String bread;
+    private int size;
+    private boolean toasted;
+
+    public Sandwich(String bread, int size, boolean toasted) {
+        this.bread = bread;
+        this.size = size;
+        this.toasted = toasted;
+    }
+
+    public String getSummary() {
+        return size + "\" " + bread + " sandwich" +
+                (toasted ? " toasted" : " not toasted");
+    }
+}
