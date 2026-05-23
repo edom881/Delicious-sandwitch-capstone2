3b24e9da24e12fce48cc08a7e3a270afca751333
edomg <edomgezahegn54@gmail.com>
2026-05-23 14:01:36 -0500

    Add ArrayList fields to Sandwich class for meats, cheeses, toppings, and sauces customization

--- src/main/java/com/pluralsight/models/Sandwich.java
+++ src/main/java/com/pluralsight/models/Sandwich.java
@@ -1,9 +1,16 @@
 package com.pluralsight.models;
 
+import java.util.ArrayList;
+
 public class Sandwich {
     private String bread;
     private int size;
     private boolean toasted;
+
+    private ArrayList<String> meats;
+    private ArrayList<String> cheeses;
+    private ArrayList<String> toppings;
+    private ArrayList<String> sauces;
 
     public Sandwich(String bread, int size, boolean toasted) {
         this.bread = bread;
