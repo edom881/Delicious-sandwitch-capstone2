b223959e7adaf45ebd42d7287bdebe06fa060aeb
edomg <edomgezahegn54@gmail.com>
2026-05-23 18:43:48 -0500

    Add detailed code comments throughout Sandwich class to improve readability, maintainability, and presentation clarity

--- src/main/java/com/pluralsight/models/Sandwich.java
+++ src/main/java/com/pluralsight/models/Sandwich.java
@@ -3,45 +3,58 @@ package com.pluralsight.models;
 import java.util.ArrayList;
 
 public class Sandwich {
+    // stores bread type selected by customer
     private String bread;
+
+    // stores sandwich size (4, 8, or 12 inches)
     private int size;
+
+    // stores toasted option
     private boolean toasted;
 
+    // stores premium meat toppings
     private ArrayList<String> meats;
+
+    // stores premium cheese toppings
     private ArrayList<String> cheeses;
+
+    // stores regular toppings
     private ArrayList<String> toppings;
+
+    // stores sauces selected by customer
     private ArrayList<String> sauces;
 
+    // constructor used to create a sandwich object
     public Sandwich(String bread, int size, boolean toasted) {
         this.bread = bread;
         this.size = size;
         this.toasted = toasted;
 
+        // initialize ArrayLists for sandwich ingredients
         this.meats = new ArrayList<>();
         this.cheeses = new ArrayList<>();
         this.toppings = new ArrayList<>();
         this.sauces = new ArrayList<>();
 
     }
+    // adds meat to sandwich
     public void addMeat(String meat) {
         meats.add(meat);
     }
-
+    // adds cheese to sandwich
     public void addCheese(String cheese) {
         cheeses.add(cheese);
     }
-
+    // adds regular topping to sandwich
     public void addTopping(String topping) {
         toppings.add(topping);
     }
-
+    // adds sauce to sandwich
     public void addSauce(String sauce) {
         sauces.add(sauce);
     }
 
-    public String getSummary() {
-        return size + "\" " + bread + " sandwich" +
-                (toasted ? " toasted" : " not toasted");
     }
 
-}
+
+
