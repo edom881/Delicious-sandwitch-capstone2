54c961bf94b9d95097bb74029aa7d33c66bbfd5d
edomg <edomgezahegn54@gmail.com>
2026-05-23 13:38:16 -0500

    Add order screen menu

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -21,7 +21,7 @@ public class Application {
             switch (choice) {
 
                 case "1":
-                    System.out.println("Starting new order...");
+                    System.out.println("showOrderScreen();");
                     break;
 
                 case "0":
@@ -33,8 +33,47 @@ public class Application {
                     System.out.println("Invalid choice. Try again.");
             }
         }
-    }
 
     }
+
+    public static void showOrderScreen() {
+
+        boolean ordering = true;
+
+        while (ordering) {
+            System.out.println("\n=== Order Screen ===");
+            System.out.println("1) Add Sandwich");
+            System.out.println("2) Add Drink");
+            System.out.println("3) Add Chips");
+            System.out.println("4) Checkout");
+            System.out.println("0) Cancel Order");
+            System.out.print("Enter choice: ");
+
+            String orderChoice = scanner.nextLine();
+
+            switch (orderChoice) {
+                case "1":
+                    System.out.println("Add Sandwich selected.");
+                    break;
+                case "2":
+                    System.out.println("Add Drink selected.");
+                    break;
+                case "3":
+                    System.out.println("Add Chips selected.");
+                    break;
+                case "4":
+                    System.out.println("Checkout selected.");
+                    break;
+                case "0":
+                    ordering = false;
+                    System.out.println("Order canceled. Returning home.");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Try again.");
+            }
+        }
+    }
+
+}
 
 
