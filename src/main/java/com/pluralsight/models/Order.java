776b92252f85163ade35b72d73f55b914cd4376e
edomg <edomgezahegn54@gmail.com>
2026-05-24 15:50:15 -0500

    Create Order class with ArrayLists to store sandwiches, drinks, and chips for customer orders

---
+++ src/main/java/com/pluralsight/models/Order.java
@@ -0,0 +1,17 @@
+package com.pluralsight.models;
+
+import java.util.ArrayList;
+
+public class Order {
+
+        // stores all sandwiches in the order
+        private ArrayList<Sandwich> sandwiches;
+
+        // stores all drinks in the order
+        private ArrayList<Drink> drinks;
+
+        // stores all chips in the order
+        private ArrayList<Chips> chips;
+    }
+
+
