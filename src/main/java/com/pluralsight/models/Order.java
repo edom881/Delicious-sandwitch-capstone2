99722e405bf677e8c9ffcc074f87d5be5d30c089
edomg <edomgezahegn54@gmail.com>
2026-05-24 15:51:38 -0500

    Initialize Order collections to support multiple sandwiches, drinks, and chips per customer order

--- src/main/java/com/pluralsight/models/Order.java
+++ src/main/java/com/pluralsight/models/Order.java
@@ -4,14 +4,23 @@ import java.util.ArrayList;
 
 public class Order {
 
-        // stores all sandwiches in the order
-        private ArrayList<Sandwich> sandwiches;
+    // stores all sandwiches in the order
+    private ArrayList<Sandwich> sandwiches;
 
-        // stores all drinks in the order
-        private ArrayList<Drink> drinks;
+    // stores all drinks in the order
+    private ArrayList<Drink> drinks;
 
-        // stores all chips in the order
-        private ArrayList<Chips> chips;
+    // stores all chips in the order
+    private ArrayList<Chips> chips;
+
+    // constructor initializes order collections
+    public Order() {
+
+        this.sandwiches = new ArrayList<>();
+        this.drinks = new ArrayList<>();
+        this.chips = new ArrayList<>();
     }
+
+}
 
 
