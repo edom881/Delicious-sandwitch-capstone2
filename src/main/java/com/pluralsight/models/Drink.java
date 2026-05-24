afd59b482b2679015981551ef3dcd89fb1002b79
edomg <edomgezahegn54@gmail.com>
2026-05-24 15:29:08 -0500

    Implement Drink constructor with automatic drink pricing logic based on selected drink size

--- src/main/java/com/pluralsight/models/Drink.java
+++ src/main/java/com/pluralsight/models/Drink.java
@@ -9,4 +9,22 @@ public class Drink {
 
     // stores drink price
     private double price;
+
+    // constructor used to create drink object
+    public Drink(String flavor, String size) {
+
+        this.flavor = flavor;
+        this.size = size;
+
+        // sets drink price based on size
+        if (size.equalsIgnoreCase("small")) {
+            price = 2.00;
+        }
+        else if (size.equalsIgnoreCase("medium")) {
+            price = 2.50;
+        }
+        else if (size.equalsIgnoreCase("large")) {
+            price = 3.00;
+        }
+    }
 }
