36d7dfea716ef8ce83393c408b3ee1a62e5029ac
edomg <edomgezahegn54@gmail.com>
2026-05-24 16:49:38 -0500

    Implement chips ordering flow to collect chip type from customer input and Connect chips input flow to Order class so selected chips are stored in the active order

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -3,6 +3,7 @@ package com.pluralsight;
 import com.pluralsight.models.Order;
 import com.pluralsight.models.Drink;
 import java.util.Scanner;
+import com.pluralsight.models.Chips;
 
 public class Application {
 
@@ -70,7 +71,13 @@ public class Application {
                     System.out.println("Drink added to order.");
                     break;
                 case "3":
-                    System.out.println("Add Chips selected.");
+                    System.out.print("Enter chip type: ");
+                    String chipType = scanner.nextLine();
+
+                    Chips chips = new Chips(chipType);
+                    order.addChips(chips);
+
+                    System.out.println("Chips added to order.");
                     break;
                 case "4":
                     System.out.println("Checkout selected.");
