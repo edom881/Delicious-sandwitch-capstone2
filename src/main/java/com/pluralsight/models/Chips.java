0cdca932603ae21deefff208ae40f73de8bcad56
edomg <edomgezahegn54@gmail.com>
2026-05-24 15:33:44 -0500

    Create Chips class with fields for chip type and fixed product pricing

---
+++ src/main/java/com/pluralsight/models/Chips.java
@@ -0,0 +1,11 @@
+package com.pluralsight.models;
+
+public class Chips {
+
+    // stores chip type selected by customer
+    private String type;
+
+    // stores chips price
+    private double price;
+}
+
