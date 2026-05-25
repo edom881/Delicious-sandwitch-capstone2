b81a7ecf8262e1f5de0490857ff307a9761a9f74
edomg <edomgezahegn54@gmail.com>
2026-05-25 17:04:37 -0500

    Add timestamp-based receipt filename generation using yyyyMMdd-HHmmss format

--- src/main/java/com/pluralsight/util/ReceiptWriter.java
+++ src/main/java/com/pluralsight/util/ReceiptWriter.java
@@ -10,6 +10,30 @@ import java.time.format.DateTimeFormatter;
 
 
 public class ReceiptWriter {
+    // saves completed order to receipt file
+    public static void saveReceipt(Order order) {
+
+        String fileName = "receipts/" + generateFileName();
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+
+            writer.write(order.getOrderSummary());
+
+            writer.close();
+
+            System.out.println("Receipt saved: " + fileName);
+
+        } catch (IOException e) {
+            System.out.println("Error saving receipt.");
+        }
+    }
+
+    // creates receipt filename using current date and time
+    private static String generateFileName() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
+        return LocalDateTime.now().format(formatter) + ".txt";
+    }
 
 
 }
