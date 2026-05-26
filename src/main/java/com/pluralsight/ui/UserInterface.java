162faa7fba3ce0c352d21d7ad7504f4d7cf2958b
edomg <edomgezahegn54@gmail.com>
2026-05-25 21:47:37 -0500

    Add home screen and order screen methods to UserInterface for cleaner menu navigation

--- src/main/java/com/pluralsight/ui/UserInterface.java
+++ src/main/java/com/pluralsight/ui/UserInterface.java
@@ -1,5 +1,40 @@
 package com.pluralsight.ui;
 
+import java.util.Scanner;
+
 public class UserInterface {
+    // scanner reads user input from the console
+    private Scanner scanner;
+
+    // constructor creates scanner object
+    public UserInterface() {
+        scanner = new Scanner(System.in);
+    }
+
+    // displays home screen menu
+    public String showHomeScreen() {
+
+        System.out.println("\n=== DELI-cious Sandwich Shop ===");
+        System.out.println("1) New Order");
+        System.out.println("0) Exit");
+        System.out.print("Enter choice: ");
+
+        return scanner.nextLine();
+    }
+
+    // displays order screen menu
+    public String showOrderScreen() {
+
+        System.out.println("\n=== Order Screen ===");
+        System.out.println("1) Add Sandwich");
+        System.out.println("2) Add Drink");
+        System.out.println("3) Add Chips");
+        System.out.println("4) Checkout");
+        System.out.println("0) Cancel Order");
+        System.out.print("Enter choice: ");
+
+        return scanner.nextLine();
+    }
+
 
 }
