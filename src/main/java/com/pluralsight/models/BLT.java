ab2222f99d400d3ef47cb93270cad50c90bbceb0
edomg <edomgezahegn54@gmail.com>
2026-05-27 13:46:49 -0500

    Implement automatic BLT sandwich configuration with predefined toppings, cheese, sauce, and pricing behavior

--- src/main/java/com/pluralsight/models/BLT.java
+++ src/main/java/com/pluralsight/models/BLT.java
@@ -1,5 +1,18 @@
 package com.pluralsight.models;
 public class BLT extends Sandwich {
+    // creates a default BLT sandwich
+    public BLT() {
+
+        super("white", 8, true);
+
+        addMeat("bacon", false);
+
+        addCheese("cheddar", false);
+
+        addTopping("lettuce");
+        addTopping("tomatoes");
+
+        addSauce("ranch");
 
 
     }
