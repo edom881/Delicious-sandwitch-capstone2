7579c2dcf496a17f38ef4e98b857e06f893e6fad
edomg <edomgezahegn54@gmail.com>
2026-05-27 11:47:45 -0500

    Add unit test for sandwich pricing with extra meat and extra cheese selections

---
+++ src/test/java/com/pluralsight/models/SandwichTest.java
@@ -0,0 +1,19 @@
+package com.pluralsight.models;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class SandwichTest {
+    @Test
+    public void getSandwichPrice_shouldCalculateCorrectPrice() {
+        // arrange
+        Sandwich sandwich = new Sandwich("white", 8, true);
+        sandwich.addMeat("bacon", true);
+        sandwich.addCheese("cheddar", true);
+        // act
+        double price = sandwich.getPrice();
+        // assert
+        assertEquals(12.10, price, 0.01);
+    }
+
+    }
+
