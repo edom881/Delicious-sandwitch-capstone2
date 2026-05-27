d9bd58b609d0bfeb926ce98315ac1a7771d1498d
edomg <edomgezahegn54@gmail.com>
2026-05-26 22:23:13 -0500

    Refactor Order class to use shared OrderItem interface collection for polymorphic order item storage

--- src/main/java/com/pluralsight/models/Order.java
+++ src/main/java/com/pluralsight/models/Order.java
@@ -4,14 +4,8 @@ import java.util.ArrayList;
 
 public class Order {
 
-    // stores all sandwiches in the order
-    private ArrayList<Sandwich> sandwiches;
-
-    // stores all drinks in the order
-    private ArrayList<Drink> drinks;
-
-    // stores all chips in the order
-    private ArrayList<Chips> chips;
+    // stores all order items
+    private ArrayList<OrderItem> items;
 
     // constructor initializes order collections
     public Order() {
